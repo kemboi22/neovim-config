9d3d8cc0b6bc003d803bf47234a9be5fecf80dc8
kemboi22 <kemboielvis22@gmail.com>
2024-10-26 02:17:31 +0300

    feat: added linting

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,37 +1,35 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local opts = {
   sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.phpcbf,
     null_ls.builtins.diagnostics.phpstan,
-                    null_ls.builtins.formatting.pint,
-    -- null_ls.builtins.formatting.pylint,
+    null_ls.builtins.formatting.pint,
+    null_ls.builtins.formatting.pretty_php,
     null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.gofumpt,
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.blade_formatter,
     -- null_ls.builtins.formatting.
   },
-  on_attach = function (client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
         group = augroup,
-          buffer = bufnr,
-      })
+        buffer = bufnr,
+      }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
-          buffer = bufnr,
-        callback =  function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end
-
-        
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
       })
     end
-  end
+  end,
 }
 
 return opts
