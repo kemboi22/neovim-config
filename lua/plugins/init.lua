db7840d308046b497da50776f363396427c3c93e
kemboi22 <kemboielvis22@gmail.com>
2024-10-26 02:08:24 +0300

    feat: added linting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,32 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = { "BufReadPre", "BufNewFile" },
+    -- event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
+    config = function()
+      local conform = require "conform"
+
+      conform.setup {
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "isort", "black" },
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          vue = { "prettier" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        },
+      }
+    end,
   },
   {
     "williamboman/mason.nvim",
@@ -38,15 +62,19 @@ return {
         "tailwindcss-language-server",
         "vue-language-server",
         "yaml-language-server",
-        "intelephense", 
+        "intelephense",
         "python-lsp-server",
-        "pylint"
+        "pylint",
+        "stylua",
+        "black",
+        "isort",
+        "eslint_d",
       },
-      automatic_installation = true
+      automatic_installation = true,
     },
-    config = function ()
+    config = function()
       require("mason").setup()
-    end
+    end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -60,9 +88,9 @@ return {
   {
     "nvimtools/none-ls.nvim",
     event = "VeryLazy",
-    opts = function ()
-      return require "custom.configs.null-ls"      
-    end
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
   },
   {
     "windwp/nvim-ts-autotag",
@@ -70,82 +98,129 @@ return {
       "javascript",
       "typescript",
       "html",
-      "vue"
+      "vue",
     },
-    config = function ()
-      require("nvim-ts-autotag").setup()      
-    end
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
   },
-   {
-   	"nvim-treesitter/nvim-treesitter",
-   	opts = {
-   		ensure_installed = {
-   			"vim", "lua", "vimdoc",
-        "html", "css", "javascript", "typescript", "vue", "php", "bash", "json", "dot", "go", "gitcommit", "gitignore", "gomod", "goctl", "phpdoc",
-        "python", "dockerfile", "comment"
-   		},
-   	},
-   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "vue",
+        "php",
+        "bash",
+        "json",
+        "dot",
+        "go",
+        "gitcommit",
+        "gitignore",
+        "gomod",
+        "goctl",
+        "phpdoc",
+        "python",
+        "dockerfile",
+        "comment",
+      },
+    },
+  },
   {
     "adalessa/laravel.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "tpope/vim-dotenv",
-    "MunifTanjim/nui.nvim",
-    "nvimtools/none-ls.nvim",
-  },
-  cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-  keys = {
-    { "<leader>la", ":Laravel artisan<cr>" },
-    { "<leader>lr", ":Laravel routes<cr>" },
-    { "<leader>lm", ":Laravel related<cr>" },
-  },
-  event = { "VeryLazy" },
-  opts = {
-    features = {
-      null_ls = {
-        enable = true,
-      },
-      route_info = {
-        enable = true,         --- to enable the laravel.nvim virtual text
-        position = 'right',    --- where to show the info (available options 'right', 'top')
-        middlewares = true,    --- wheather to show the middlewares section in the info
-        method = true,         --- wheather to show the method section in the info
-        uri = true             --- wheather to show the uri section in the info
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    opts = {
+      features = {
+        null_ls = {
+          enable = true,
+        },
+        route_info = {
+          enable = true, --- to enable the laravel.nvim virtual text
+          position = "right", --- where to show the info (available options 'right', 'top')
+          middlewares = true, --- wheather to show the middlewares section in the info
+          method = true, --- wheather to show the method section in the info
+          uri = true, --- wheather to show the uri section in the info
+        },
       },
     },
-  },
-  config = true,
+    config = true,
   },
   {
     "rcarriga/nvim-notify",
     config = function()
-        local notify = require("notify")
-        -- this for transparency
-        notify.setup({ background_colour = "#000000" })
-        -- this overwrites the vim notify function
-        vim.notify = notify.notify
-    end
-  },{
+      local notify = require "notify"
+      -- this for transparency
+      notify.setup { background_colour = "#000000" }
+      -- this overwrites the vim notify function
+      vim.notify = notify.notify
+    end,
+  },
+  {
     "ray-x/go.nvim",
     dependencies = {
-       "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-    require("go").setup()
-  end,
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()'
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
   },
   {
     "dariuscorvus/tree-sitter-surrealdb.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    -- setup step
-    require("tree-sitter-surrealdb").setup()
-  end,
-  }
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      -- setup step
+      require("tree-sitter-surrealdb").setup()
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require "lint"
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        python = { "pylint" },
+        vue = { "eslint_d" },
+        go = { "golangcilint" },
+        php = { "phpstan", "php", "phpcs" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
 }
