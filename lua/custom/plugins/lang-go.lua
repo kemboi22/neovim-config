6c1c7a50365bad9a69467bf9951a3197243a3c17
kemboi22 <kemboielvis22@gmail.com>
2024-11-05 12:15:04 +0300

    feat: few added extra go support

--- lua/custom/plugins/lang-go.lua
+++ lua/custom/plugins/lang-go.lua
@@ -1,6 +1,6 @@
 local constants = require "custom.utils.constants"
 local langUtils = require "custom.utils.lang"
-
+local util = require "lspconfig/util"
 return {
   {
     enabled = not constants.first_install,
@@ -25,7 +25,63 @@ return {
               desc = "Debug Nearest (Go)",
             },
           },
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
         },
+      },
+      setup = {
+        gopls = function(_, opts)
+          -- workaround for gopls not supporting semanticTokensProvider
+          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          LazyVim.lsp.on_attach(function(client, _)
+            if not client.server_capabilities.semanticTokensProvider then
+              local semantic = client.config.capabilities.textDocument.semanticTokens
+              client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                  tokenTypes = semantic.tokenTypes,
+                  tokenModifiers = semantic.tokenModifiers,
+                },
+                range = true,
+              }
+            end
+          end, "gopls")
+          -- end workaround
+        end,
       },
     },
   },
@@ -39,9 +95,69 @@ return {
     "williamboman/mason.nvim",
     optional = true,
     opts = function(_, opts)
+      ensure_installed = {
+        "go-debug-adapter",
+        "gofumpt",
+        "goimports",
+        "goimports-reviser",
+        "golangci-lint",
+        "golangci-lint-langserver",
+        "golines",
+        "gomodifytags",
+        "gopls",
+      }
+
       opts.ensure_installed = opts.ensure_installed or {}
       langUtils.remove_str_from_list(opts.ensure_installed, "gomodifytags")
       langUtils.remove_str_from_list(opts.ensure_installed, "impl")
     end,
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require "lint"
+
+      lint.linters_by_ft = {
+        go = { "golangcilint" },
+      }
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        go = { "goimports_reviser", "gofumpt", "golines" },
+      },
+    },
+  },
+  {
+    "echasnovski/mini.icons",
+    opts = {
+      file = {
+        [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
+      },
+      filetype = {
+        gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
+      },
+    },
   },
 }
--- lua/custom/plugins/linting.lua
+++ lua/custom/plugins/linting.lua
@@ -94,6 +94,19 @@ return {
         group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
         callback = M.debounce(100, M.lint),
       })
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>fe", function ()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
     end,
   },
 }
