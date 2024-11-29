c21beef5fa6ce74a8dfed87b28503a75a87199e2
kemboi22 <kemboielvis22@gmail.com>
2024-11-30 01:48:58 +0300

    feat: added java support

---
+++ lua/custom/plugins/lang-java.lua
@@ -0,0 +1,56 @@
+local constants = require "custom.utils"
+return {
+  {
+    enabled = not constants.first_install,
+    import = "lazyvim.plugins.extras.lang.java",
+  },
+  {
+    "nvim-java/nvim-java",
+    config = false,
+    dependencies = {
+      {
+        "neovim/nvim-lspconfig",
+        opts = {
+          servers = {
+            -- Your JDTLS configuration goes here
+            jdtls = {
+              -- settings = {
+              --   java = {
+              --     configuration = {
+              --       runtimes = {
+              --         {
+              --           name = "JavaSE-23",
+              --           path = "/usr/local/sdkman/candidates/java/23-tem",
+              --         },
+              --       },
+              --     },
+              --   },
+              -- },
+            },
+          },
+          setup = {
+            jdtls = function()
+              -- Your nvim-java configuration goes here
+              require("java").setup {
+                -- root_markers = {
+                --   "settings.gradle",
+                --   "settings.gradle.kts",
+                --   "pom.xml",
+                --   "build.gradle",
+                --   "mvnw",
+                --   "gradlew",
+                --   "build.gradle",
+                --   "build.gradle.kts",
+                -- },
+              }
+            end,
+          },
+        },
+      },
+      {
+        "williamboman/mason.nvim",
+        opts = { ensure_installed = { "jdtls" } }
+      }
+    },
+  },
+}
