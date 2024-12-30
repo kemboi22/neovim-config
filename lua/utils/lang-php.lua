91cedd91e6087659208868ca44397f583382794c
kemboi22 <kemboielvis22@gmail.com>
2024-12-30 18:55:44 +0300

    feat: added langs and lsp

--- lua/utils/lang-php.lua
+++ lua/utils/lang-php.lua
@@ -33,6 +33,10 @@ return {
   },
   {
     "ricardoramirezr/blade-nav.nvim",
+    requires = {
+      "hrsh7th/nvim-cmp", -- if using nvim-cmp
+      { "ms-jpq/coq_nvim", branch = "coq" }, -- if using coq
+    },
     ft = { "blade", "php" }, -- optional, improves startup time
     opts = {
       close_tag_on_complete = true, -- default: true
