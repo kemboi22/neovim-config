4be9e77963fe4489d9bd8ab8bbc10a75b1e85857
kemboi22 <kemboielvis22@gmail.com>
2025-01-06 13:58:06 +0300

    fix: few changes

--- lua/tools/tools.lua
+++ lua/tools/tools.lua
@@ -6,4 +6,12 @@ return {
     event = "VeryLazy",
     enabled = vim.fn.has "nvim-0.10.0" == 1,
   },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Ensure treesitter is installed
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
