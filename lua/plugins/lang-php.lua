76a6cb4625d07c66089e81135d9f205049f6c980
kemboi22 <kemboielvis22@gmail.com>
2025-02-11 13:07:36 +0300

    added blade and injections

--- lua/plugins/lang-php.lua
+++ lua/plugins/lang-php.lua
@@ -42,4 +42,163 @@ return {
       close_tag_on_complete = true, -- default: true
     },
   },
+  {
+
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(
+        opts.sources,
+        nls.builtins.formatting.phpcsfixer.with({
+          extra_args = {
+            "--rules=@PSR12",
+            "--using-cache=no",
+          },
+        })
+      )
+      table.insert(
+        opts.sources,
+        nls.builtins.formatting.prettier.with({
+          filetypes = { "blade" },
+        })
+      )
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = { "phpcs" },
+      },
+      linters = {
+        phpcs = {
+          args = {
+            "--standard=PSR12",
+            "--report=json",
+            "-q",
+            "--runtime-set",
+            "ignore_warnings_on_exit",
+            "1",
+          },
+        },
+        phpstan = {
+          args = {
+            "analyze",
+            "--error-format",
+            "raw",
+            "--no-progress",
+            "--level",
+            "5",
+          },
+        },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Configure PHP LSP (Intelliphense)
+        intelephense = {
+          init_options = {
+            licenceKey = nil, -- Set your license key if you have one
+          },
+          settings = {
+            intelephense = {
+              environment = {
+                phpVersion = "8.2", -- Adjust based on your PHP version
+              },
+              files = {
+                maxSize = 5000000,
+              },
+              format = {
+                enable = true,
+              },
+              stubs = {
+                "apache",
+                "bcmath",
+                "bz2",
+                "calendar",
+                "com_dotnet",
+                "Core",
+                "curl",
+                "date",
+                "dba",
+                "dom",
+                "enchant",
+                "exif",
+                "fileinfo",
+                "filter",
+                "fpm",
+                "ftp",
+                "gd",
+                "gettext",
+                "gmp",
+                "hash",
+                "iconv",
+                "imap",
+                "intl",
+                "json",
+                "ldap",
+                "libxml",
+                "mbstring",
+                "mcrypt",
+                "mysql",
+                "mysqli",
+                "oci8",
+                "openssl",
+                "pcntl",
+                "pcre",
+                "PDO",
+                "pdo_mysql",
+                "Phar",
+                "readline",
+                "recode",
+                "Reflection",
+                "regex",
+                "session",
+                "shmop",
+                "SimpleXML",
+                "soap",
+                "sockets",
+                "sodium",
+                "SPL",
+                "standard",
+                "superglobals",
+                "sysvmsg",
+                "sysvsem",
+                "sysvshm",
+                "tidy",
+                "tokenizer",
+                "xml",
+                "xmlreader",
+                "xmlrpc",
+                "xmlwriter",
+                "xsl",
+                "Zend OPcache",
+                "zip",
+                "zlib",
+                "wordpress",
+                "phpunit",
+                "laravel",
+              },
+            },
+          },
+        },
+        -- Configure Phpactor as alternative
+        phpactor = {
+          cmd = { "phpactor", "language-server" },
+          filetypes = { "php" },
+          root_dir = require("lspconfig.util").root_pattern("composer.json", ".git"),
+          init_options = {
+            ["language_server_phpstan.enabled"] = true,
+            ["language_server_psalm.enabled"] = true,
+          },
+        },
+      },
+    },
+  },
 }
--- lua/plugins/ondedark.lua
+++ lua/plugins/ondedark.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("onedarkpro").setup({
       options = {
-        transparency = true,
+        transparency = false,
       },
     })
   end,
