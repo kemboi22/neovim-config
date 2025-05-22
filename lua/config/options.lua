7c1ba668768f9cef204223b867a85b0896428f46
Kemboi Elvis <kemboielvis22@gmail.com>
2025-05-22 16:59:50 +0300

    feat: changed to intelephense

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "snacks"
-vim.g.lazyvim_php_lsp = "phpactor"
+vim.g.lazyvim_php_lsp = "intelephense"
 vim.env.PHP_CS_FIXER_IGNORE_ENV = "1"
